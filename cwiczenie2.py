5499beadf99a151893cca563d7cb834cb48c108c
Rafal Podkanski <rafal.podkanski@gmail.com>
2023-10-24 14:10:12 +0200

    zadanie1

--- cwiczenie2.py
+++ cwiczenie2.py
@@ -13,16 +13,18 @@ class bussiness_card:
         return f'{self.name} {self.surname} {self.email}'
 
 def person():
-    fake_name = fake.name()
+    fake_name_and_surname = fake.name()
+    splitted_name = fake_name_and_surname.split(" ")
+    fake_name = splitted_name[0]
+    fake_surname = splitted_name[1]
     fake_company_name = fake.company()
     fake_position = fake.job()
     fake_email= fake.email()
-    fake_person = bussiness_card(name=fake_name, surname="", company_name=fake_company_name, position=fake_position, email=fake_email)
+    fake_person = bussiness_card(name=fake_name, surname=fake_surname, company_name=fake_company_name, position=fake_position, email=fake_email)
     return fake_person
 
 for i in range(5):
     print(person())
-
 
 
 
---
+++ zadanie_wizytowki.py
@@ -0,0 +1,74 @@
+from faker import Faker
+fake = Faker()
+ 
+class BaseContact:
+   
+    def __init__(self, name, surname, phone_number, email):
+        self.name = name
+        self.surname = surname
+        self.phone_number = phone_number
+        self.email = email
+       
+    @property
+    def name_length(self):
+        return len(self.name) + len(self.surname)
+   
+    def __str__(self):
+        return f'{self.name} {self.surname} {self.phone_number} {self.email}'
+       
+    def contact(self):
+        return f"Wybieram numer {self.phone_number} i dzwonię do {self.name} {self.surname}"
+   
+class BussinessContact(BaseContact):
+   
+    def __init__(self,position,company_name,bussiness_number, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.position = position
+        self.company_name = company_name
+        self.bussiness_number = bussiness_number
+       
+    def __str__(self):
+        return f'{self.name} {self.surname} {self.phone_number} {self.email} {self.position} {self.company_name} {self.bussiness_number}'
+   
+    def contact(self):
+        return f"Wybieram numer {self.bussiness_number} i dzwonię do {self.name} {self.surname}"
+ 
+def create_contacts(type,amount):
+    contact_list = []
+    if type == "base":
+        for i in range(amount):
+            fake_name_surname = fake.name()
+            fake_name_surname = fake_name_surname.split(" ")
+            fake_name = fake_name_surname[0]
+            fake_surname = fake_name_surname[1]
+            fake_phone_number = fake.phone_number()
+            fake_email= fake.email()
+            fake_person = BaseContact(name=fake_name, surname=fake_surname, phone_number=fake_phone_number, email=fake_email)
+            contact_list.append(fake_person)
+        return contact_list
+    elif type == "bussiness":
+        for i in range(amount):
+            fake_name_surname = fake.name()
+            fake_name_surname = fake_name_surname.split(" ")
+            fake_name = fake_name_surname[0]
+            fake_surname = fake_name_surname[1]
+            fake_phone_number = fake.phone_number()
+            fake_email= fake.email()
+            fake_position = fake.job()
+            fake_company = fake.company()
+            fake_bussiness_number = fake.phone_number()
+            fake_person = BussinessContact(name=fake_name, surname=fake_surname, phone_number=fake_phone_number, email=fake_email, position=fake_position,company_name=fake_company, bussiness_number=fake_bussiness_number)
+            contact_list.append(fake_person)
+        return contact_list
+    else:
+        print("wrong type. try base or bussiness")
+       
+fake_basic_contacts = create_contacts("base",2)
+fake_bussiness_contacts = create_contacts("bussiness",2)
+print(fake_basic_contacts[0])
+print(fake_bussiness_contacts[0])
+print(fake_basic_contacts[0].contact())
+print(fake_bussiness_contacts[0].contact())
+print("---")
+print(fake_basic_contacts[0].name_length)
+print(fake_bussiness_contacts[0].name_length)
